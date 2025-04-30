20bc585ce9501fbfc9841f1fe6bfb4605b0bf136
Shresth Mishra <shresthmishr@gmail.com>
2025-05-01 03:21:09 +0530

    One more question.

--- Week 1/Day 1.py
+++ Week 1/Day 1.py
@@ -7,3 +7,11 @@ class Solution:
             if len(str(num)) % 2 == 0:
                 count += 1
         return count
+
+#2 Missing Number in the Range
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        n = len(nums)
+        expected_sum = (n * (n + 1)) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
