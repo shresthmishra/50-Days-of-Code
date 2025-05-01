67fab3b29d7512283cd4e80cde9833562706dea8
Shresth Mishra <shresthmishr@gmail.com>
2025-05-01 19:02:10 +0530

    Better formatting

--- Week 1/Day 1.py
+++ Week 1/Day 1.py
@@ -1,17 +1,26 @@
-#1 Find Numbers with Even Number of Digits
+print("Challenge 1: Find Numbers with Even Number of Digits\n")
 
-class Solution:
+class EvenDigits:
     def findNumbers(self, nums: list[int]) -> int:
         count = 0
         for num in nums:
             if len(str(num)) % 2 == 0:
                 count += 1
         return count
+solution = EvenDigits()
+nums = [1, 22, 333, 4444, 55555]
+result = solution.findNumbers(nums)
+print(f"Total count of numbers with even digit length: {result}")
 
-#2 Missing Number in the Range
-class Solution:
+print("\nChallenge 2: Missing Number in the Range")
+
+class MissingNum:
     def missingNumber(self, nums: list[int]) -> int:
         n = len(nums)
         expected_sum = (n * (n + 1)) // 2
         actual_sum = sum(nums)
         return expected_sum - actual_sum
+missing_num = MissingNum()
+nums = [7, 0, 1]
+result = missing_num.missingNumber(nums)
+print(f"The missing number is: {result}")
