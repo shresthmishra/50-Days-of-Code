102b1c3e639269cd124b2471ff0faa91d315b6f4
Shresth Mishra <shresthmishr@gmail.com>
2025-05-02 16:15:53 +0530

    Update

--- Week 1/Day 1.py
+++ Week 1/Day 1.py
@@ -1,1 +1,16 @@
 print("\n\nDay 1\n")
+print("Challenge: Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.\n")
+
+class SecondLargest:
+    def getSecondLargest(self, arr):
+        if not arr or len(arr) < 2:
+            return -1
+        unique_elements = sorted(list(set(arr)), reverse=True)
+        if len(unique_elements) < 2:
+            return -1
+        else:
+            return unique_elements[1]
+solution = SecondLargest()
+numbers = [2, 5, 4, 3, 7]
+second_largest = solution.getSecondLargest(numbers)
+print(f"The second largest element in {numbers} is: {second_largest}")
