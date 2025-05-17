a14f6f7bd1a52647f25855a4acb4da4fcb942162
Shresth Mishra <shresthmishr@gmail.com>
2025-05-17 09:51:06 +0530

    Update

--- Week 3/Day 20.py
+++ Week 3/Day 20.py
@@ -1,4 +1,5 @@
 print("\n\nDay 19\n")
+
 print("\nChallenge: Two Sum")
 print("Context: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.\n\n")
 from typing import List
