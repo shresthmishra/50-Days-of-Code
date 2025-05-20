08d098a113dd653f00c8afc68f2d0ed7dc7d570b
Shresth Mishra <shresthmishr@gmail.com>
2025-05-20 10:42:25 +0530

    Update

--- Week 4/Day 24.py
+++ Week 4/Day 24.py
@@ -1,4 +1,20 @@
 print("\n\nDay 24\n")
 
-print("\nChallenge:")
-print("Context:")
+print("\nChallenge: Pascal's Triangle II")
+print("Context: Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.\n\n")
+
+from typing import List
+
+class Pascal:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex):
+            for j in range(i, 0, -1):
+                row[j] = row[j] + row[j-1]
+        
+        return row
+
+solution = Pascal()
+row_index = 3
+pascal_row = solution.getRow(row_index)
+print(f"The {row_index}th row of Pascal's Triangle is: {pascal_row}")
