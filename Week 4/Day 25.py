dda6033253dec044e78faae82373dc7e061229d2
Shresth Mishra <shresthmishr@gmail.com>
2025-05-21 23:31:58 +0530

    Update

---
+++ Week 4/Day 25.py
@@ -0,0 +1,4 @@
+print("\n\nDay 25\n")
+
+print("\nChallenge: Majority Element")
+print("Context: Given an array nums of size n, return the majority element.\nThe majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.\n\n")
