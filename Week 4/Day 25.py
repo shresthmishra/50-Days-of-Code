f422b58dbfa9d87dd6e2ccebbed8be90d84368ab
Shresth Mishra <shresthmishr@gmail.com>
2025-05-21 23:34:43 +0530

    Update

--- Week 4/Day 25.py
+++ Week 4/Day 25.py
@@ -1,4 +1,21 @@
 print("\n\nDay 25\n")
 
 print("\nChallenge: Majority Element")
-print("Context: Given an array nums of size n, return the majority element.\nThe majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.\n\n")
+print("Context: Given an array nums of size n, return the majority element.\n\n")
+
+from typing import List
+
+class Major:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = 0
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        return candidate
+
+solution = Major()
+numbers = [3, 2, 3]
+majority_element = solution.majorityElement(numbers)
+print(f"The majority element in {numbers} is: {majority_element}")
