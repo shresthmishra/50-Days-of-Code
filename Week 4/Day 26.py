7d16931704a24c7a8e923f5989c162233c1c03e3
Shresth Mishra <shresthmishr@gmail.com>
2025-05-22 22:59:53 +0530

    Update

---
+++ Week 4/Day 26.py
@@ -0,0 +1,5 @@
+print("\n\nDay 26\n")
+
+print("\nChallenge: Search Insert Position")
+print("Context: Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.\nYou must write an algorithm with O(log n) runtime complexity.\n\n")
+
