9ee3e4611e7d0b236eda776a7f4a10d3514725a7
Shresth Mishra <shresthmishr@gmail.com>
2025-05-24 07:41:46 +0530

    Update

---
+++ Week 4/Day 27.py
@@ -0,0 +1,5 @@
+print("\n\nDay 27\n")
+
+print("\nChallenge: Move Zeroes")
+print("Context: Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.\nNote that you must do this in-place without making a copy of the array.\n\n")
+
