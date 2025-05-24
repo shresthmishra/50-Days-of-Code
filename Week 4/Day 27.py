e7ca8f2638f49c43247d7c4f0d04fc77ae92eb15
Shresth Mishra <shresthmishr@gmail.com>
2025-05-24 07:45:00 +0530

    Update

--- Week 4/Day 27.py
+++ Week 4/Day 27.py
@@ -3,3 +3,18 @@ print("\n\nDay 27\n")
 print("\nChallenge: Move Zeroes")
 print("Context: Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.\nNote that you must do this in-place without making a copy of the array.\n\n")
 
+from typing import List
+
+class Zeroes:
+    def moveZeroes(self, nums: List[int]) -> None:
+        non_zero_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_pos], nums[i] = nums[i], nums[non_zero_pos]
+                non_zero_pos += 1
+
+solution = Zeroes()
+numbers = [0, 1, 0, 3, 12]
+print(f"Original list: {numbers}")
+solution.moveZeroes(numbers)
+print(f"List after moving zeros: {numbers}")
