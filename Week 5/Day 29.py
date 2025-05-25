b2eae1dc9745ae117237e56f936343c46eceb6db
Shresth Mishra <shresthmishr@gmail.com>
2025-05-25 22:34:13 +0530

    New week!

---
+++ Week 5/Day 29.py
@@ -0,0 +1,5 @@
+print("\n\nDay 29\n")
+
+print("\nChallenge: Find First and Last Position of Element in Sorted Array")
+print("Context: Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.\nIf target is not found in the array, return [-1, -1].\nYou must write an algorithm with O(log n) runtime complexity.\n\n")
+
