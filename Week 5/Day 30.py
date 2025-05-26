1d8be858f716230c8465142107c233cf88eed323
Shresth Mishra <shresthmishr@gmail.com>
2025-05-26 21:33:27 +0530

    Update (Reached a month)

---
+++ Week 5/Day 30.py
@@ -0,0 +1,4 @@
+print("\n\nDay 30n")
+
+print("\nChallenge: Path Sum")
+print("Context: Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.\nA leaf is a node with no children.\n\n")
