d52e71a71a608e6400849c4cc6bd94fe7ed83ca5
Shresth Mishra <shresthmishr@gmail.com>
2025-05-26 22:58:01 +0530

    Update

--- Week 5/Day 30.py
+++ Week 5/Day 30.py
@@ -1,4 +1,34 @@
-print("\n\nDay 30n")
+print("\n\nDay 30\n")
 
 print("\nChallenge: Path Sum")
 print("Context: Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.\nA leaf is a node with no children.\n\n")
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+class PathSum:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        if not root.left and not root.right:
+            return targetSum == root.val
+        
+        return (self.hasPathSum(root.left, targetSum - root.val) or 
+                self.hasPathSum(root.right, targetSum - root.val))
+root = TreeNode(5)
+root.left = TreeNode(4)
+root.right = TreeNode(8)
+root.left.left = TreeNode(11)
+root.left.left.left = TreeNode(7)
+root.left.left.right = TreeNode(2)
+root.right.left = TreeNode(13)
+root.right.right = TreeNode(4)
+root.right.right.right = TreeNode(1)
+solution = PathSum()
+target = 22
+result = solution.hasPathSum(root, target)
+print(f"Does a path with sum {target} exist? {result}")
