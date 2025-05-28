b3b0191e820144244d96a1ef5e8720e0760af5b7
Shresth Mishra <shresthmishr@gmail.com>
2025-05-29 00:40:30 +0530

    Update (Day 31)

---
+++ Week 5/Day 32.py
@@ -0,0 +1,5 @@
+print("\n\nDay 31\n")
+
+print("\nChallenge: Sum of Left Leaves")
+print("Context: Given the root of a binary tree, return the sum of all left leaves.\nA leaf is a node with no children. A left leaf is a leaf that is the left child of another node.\n\n")
+
