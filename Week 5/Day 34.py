1171c1b806d492e5132ed83873060ddf27cf6551
Shresth Mishra <shresthmishr@gmail.com>
2025-05-30 12:20:35 +0530

    Day 34 code completed

--- Week 5/Day 34.py
+++ Week 5/Day 34.py
@@ -3,3 +3,29 @@ print("\n\nDay 34\n")
 print("\nChallenge: Maximum Depth of Binary Tree")
 print("Context: Given the root of a binary tree, return its maximum depth.\nA binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.\n\n")
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional
+
+class MaxDepth:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return 1 + max(left_depth, right_depth)
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+solution = MaxDepth()
+
+result = solution.maxDepth(root)
+print(f"The maximum depth of the tree is: {result}")
