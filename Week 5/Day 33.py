7ad5e41d28a38d2d5dff0ce5a01e768458d76b6f
Shresth Mishra <shresthmishr@gmail.com>
2025-05-30 12:13:39 +0530

    Day 33 code completed (fixed)

--- Week 5/Day 33.py
+++ Week 5/Day 33.py
@@ -1,4 +1,4 @@
-print("\n\nDay 32\n")
+print("\n\nDay 33\n")
 
 print("\nChallenge: Diameter of Binary Tree")
 print("Context: Given the root of a binary tree, return the length of the diameter of the tree.\nThe diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.\nThe length of a path between two nodes is represented by the number of edges between them.\n\n")
