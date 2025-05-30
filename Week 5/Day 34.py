64c8cf854059d17c938c63579db02b39aca68450
Shresth Mishra <shresthmishr@gmail.com>
2025-05-30 12:14:23 +0530

    Day 34 update

---
+++ Week 5/Day 34.py
@@ -0,0 +1,5 @@
+print("\n\nDay 34\n")
+
+print("\nChallenge: Maximum Depth of Binary Tree")
+print("Context: Given the root of a binary tree, return its maximum depth.\nA binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.\n\n")
+
