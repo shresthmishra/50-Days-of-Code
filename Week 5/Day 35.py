60d7b3f4ed7f80e50174ea7e1052c296b109558e
Shresth Mishra <shresthmishr@gmail.com>
2025-05-31 13:16:36 +0530

    Day 35 update

---
+++ Week 5/Day 35.py
@@ -0,0 +1,5 @@
+print("\n\nDay 35\n")
+
+print("\nChallenge: Binary Tree Level Order Traversal")
+print("Context: Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).")
+
