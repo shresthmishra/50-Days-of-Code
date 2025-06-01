e710fd94ddc510e162d9975f5c1d53370310f43a
Shresth Mishra <shresthmishr@gmail.com>
2025-06-01 12:15:57 +0530

    Week 6 update!

---
+++ Week 6/Day 36.py
@@ -0,0 +1,5 @@
+print("\n\nDay 36\n")
+
+print("\nChallenge: Lexicographically Smallest Palindrome")
+print("Context: You are given a string s consisting of lowercase English letters, and you are allowed to perform operations on it. In one operation, you can replace a character in s with another lowercase English letter.\n- Your task is to make s a palindrome with the minimum number of operations possible. If there are multiple palindromes that can be made using the minimum number of operations, make the lexicographically smallest one.\n- A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, string a has a letter that appears earlier in the alphabet than the corresponding letter in b.\n- Return the resulting palindrome string.")
+
