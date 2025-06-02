a274ff765b1b759c1b8a910f373c93dcefa7f2ff
Shresth Mishra <shresthmishr@gmail.com>
2025-06-02 13:54:06 +0530

    Day 37 - Update

---
+++ Week 6/Day 37.py
@@ -0,0 +1,5 @@
+print("\n\nDay 37\n")
+
+print("\nChallenge: Length of Last Word")
+print("Context: Given a string s consisting of words and spaces, return the length of the last word in the string.\nA word is a maximal substring consisting of non-space characters only.\n\n")
+
