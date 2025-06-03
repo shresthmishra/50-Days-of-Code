fde565f7534b89a8577640cce4612d100f84d96a
Shresth Mishra <shresthmishr@gmail.com>
2025-06-03 13:53:22 +0530

    Day 38 - Code completed

--- Week 6/Day 38.py
+++ Week 6/Day 38.py
@@ -3,3 +3,11 @@ print("\n\nDay 38\n")
 print("\nChallenge: Number of Segments in a String")
 print("Context: Given a string s, return the number of segments in the string.\nA segment is defined to be a contiguous sequence of non-space characters.\n\n")
 
+class Segments:
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
+        
+solution = Segments()
+s = "Hello, my name is Shresth."
+result = solution.countSegments(s)
+print(f"Number of the segments in the string s is {result}")
