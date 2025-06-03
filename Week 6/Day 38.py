2e5a7fc89f70d9dacd8ec239552a1973ac321421
Shresth Mishra <shresthmishr@gmail.com>
2025-06-03 13:46:12 +0530

    Day 38 - Update

---
+++ Week 6/Day 38.py
@@ -0,0 +1,5 @@
+print("\n\nDay 38\n")
+
+print("\nChallenge: Number of Segments in a String")
+print("Context: Given a string s, return the number of segments in the string.\nA segment is defined to be a contiguous sequence of non-space characters.\n\n")
+
