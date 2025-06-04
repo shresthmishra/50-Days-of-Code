48b6b6fdb69a8b2b2104c32b62ce228fb91cfa37
Shresth Mishra <shresthmishr@gmail.com>
2025-06-04 19:57:25 +0530

    Day 39 - Update

---
+++ Week 6/Day 39.py
@@ -0,0 +1,5 @@
+print("\n\nDay 39\n")
+
+print("\nChallenge: Reverse Vowels of a String")
+print("Context: Given a string s, reverse only all the vowels in the string and return it.\nThe vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.\n\n")
+
