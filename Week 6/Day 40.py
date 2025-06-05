004e727ab5597f30eb537044b723ada6eda11325
Shresth Mishra <shresthmishr@gmail.com>
2025-06-05 18:52:56 +0530

    Day 40 - Update

---
+++ Week 6/Day 40.py
@@ -0,0 +1,5 @@
+print("\n\nDay 40\n")
+
+print("\nChallenge: Find First Palindromic String in the Array")
+print("Context: Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".\nA string is palindromic if it reads the same forward and backward.\n\n")
+
