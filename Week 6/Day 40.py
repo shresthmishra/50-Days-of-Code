9d2614922b07eb888e9baf789cbe96b7f86197d8
Shresth Mishra <shresthmishr@gmail.com>
2025-06-05 19:01:38 +0530

    Day 40 - Code Completed

--- Week 6/Day 40.py
+++ Week 6/Day 40.py
@@ -3,3 +3,14 @@ print("\n\nDay 40\n")
 print("\nChallenge: Find First Palindromic String in the Array")
 print("Context: Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".\nA string is palindromic if it reads the same forward and backward.\n\n")
 
+class FirstPalindrome:
+    def firstPalindrome(self, words: list[str]) -> str:
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ""
+    
+solution = FirstPalindrome()
+List = ["shresth","racecar","shivam","ada"]
+result = solution.firstPalindrome(List)
+print(f"The First Palindromic String in the given Array is: {result}")
