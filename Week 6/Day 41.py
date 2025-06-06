026c0a690903b6b9b54459c9f667cd1f55914f59
Shresth Mishra <shresthmishr@gmail.com>
2025-06-06 23:44:13 +0530

    Day 41 - Code Completed

--- Week 6/Day 41.py
+++ Week 6/Day 41.py
@@ -3,3 +3,13 @@ print("\n\nDay 41\n")
 print("\nChallenge: Valid Palindrome")
 print("Context: A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.\nGiven a string s, return true if it is a palindrome, or false otherwise.\n\n")
 
+class Valid:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ''.join(char.lower() for char in s if char.isalnum())
+        return cleaned == cleaned[::-1]
+
+solution = Valid()
+string = "Hello there, good people"
+result = solution.isPalindrome(string)
+
+print(f"Is the given string ''{string}'' a valid Palindrome? {result}")
