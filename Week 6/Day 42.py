0766a6fa16168102c9242dd394bb33cf1e330b45
Shresth Mishra <shresthmishr@gmail.com>
2025-06-07 09:02:52 +0530

    Day 42 - Update

---
+++ Week 6/Day 42.py
@@ -0,0 +1,5 @@
+print("\n\nDay 42\n")
+
+print("\nChallenge: Remove All Adjacent Duplicates In String")
+print("Context: You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.\nWe repeatedly make duplicate removals on s until we no longer can.\nReturn the final string after all such duplicate removals have been made. It can be proven that the answer is unique.\n\n")
+
