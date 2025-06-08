fd6d6c3f32cef09f291c8f6ef3aa9caabc896dd6
Shresth Mishra <shresthmishr@gmail.com>
2025-06-08 17:54:06 +0530

    Final Week + Day 43 Update

---
+++ Week 7/Day 43.py
@@ -0,0 +1,5 @@
+print("\n\nDay 43\n")
+
+print("\nChallenge: Rotate String")
+print("Context: Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.\nA shift on s consists of moving the leftmost character of s to the rightmost position.\nFor example, if s = "abcde", then it will be "bcdea" after one shift.\n\n")
+
