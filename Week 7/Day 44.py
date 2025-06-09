436f3d2ce4a6ad6844a93c183d0a2c5ffbbf6cd6
Shresth Mishra <shresthmishr@gmail.com>
2025-06-09 23:35:30 +0530

    Day 44 - Update

---
+++ Week 7/Day 44.py
@@ -0,0 +1,5 @@
+print("\n\nDay 44\n")
+
+print("\nChallenge: Longest Common Prefix")
+print("Context: Write a function to find the longest common prefix string amongst an array of strings.\nIf there is no common prefix, return an empty string.\n\n")
+
