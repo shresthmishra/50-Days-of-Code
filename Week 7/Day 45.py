46a4bdaeb95a59476529701e4d01a31b3b99fe91
Shresth Mishra <shresthmishr@gmail.com>
2025-06-10 22:56:03 +0530

    Day 45 - Update

---
+++ Week 7/Day 45.py
@@ -0,0 +1,5 @@
+print("\n\nDay 45\n")
+
+print("\nChallenge: Reverse Words in a String")
+print("Context: Given an input string s, reverse the order of the words.\nA word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.\nReturn a string of the words in reverse order concatenated by a single space.\nNote that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.\n\n")
+
