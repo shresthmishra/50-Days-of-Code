204fdeedbfd7aedb3477d3f16fb8109e9027fd54
Shresth Mishra <shresthmishr@gmail.com>
2025-06-12 20:35:01 +0530

    Day 47 - Update

---
+++ Week 7/Day 47.py
@@ -0,0 +1,4 @@
+print("\n\nDay 47\n")
+
+print("\nChallenge: Maximum Number of Vowels in a Substring of Given Length")
+print("Context: Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.\nVowel letters in English are 'a', 'e', 'i', 'o', and 'u'.\n\n")
