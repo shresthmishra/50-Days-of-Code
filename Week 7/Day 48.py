4577be7b691ec0ad7fbe380060dfa558686e74b2
Shresth Mishra <shresthmishr@gmail.com>
2025-06-13 20:58:15 +0530

    Day 48 - Update

---
+++ Week 7/Day 48.py
@@ -0,0 +1,5 @@
+print("\n\nDay 48\n")
+
+print("\nChallenge: Greatest Common Divisor of Strings")
+print("Context: For two strings s and t, we say ''t divides s'' if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).\nGiven two strings str1 and str2, return the largest string x such that x divides both str1 and str2.\n\n")
+
