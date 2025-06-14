1eecb49dd8eef6f7fb6470b4b305ba485b735f7f
Shresth Mishra <shresthmishr@gmail.com>
2025-06-15 01:16:15 +0530

    Day 50 - Update

---
+++ Week 7/Day 50.py
@@ -0,0 +1,5 @@
+print("\n\nDay 50\n")
+
+print("\nChallenge: Number of 1 Bits")
+print("Context: Given a positive integer n, write a function that returns the number of in its binary representation (also known as the Hamming weight).")
+
